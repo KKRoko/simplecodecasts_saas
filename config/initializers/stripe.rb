15ee7ecc388ff3266d601cfc036812cc8100058e
Konrad Kale <konrad.kale@icloud.com>
2015-09-26 17:08:38 +0000

    Added notice and styles

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,2 +1,5 @@
-Stripe.api_key = ENV["stripe_api_key"]
-STRIPE_PUBLIC_KEY = ENV["stripe_publishable_key"]
+Rails.configuration.stripe = {
+  :publishable_key => ENV['PUBLISHABLE_KEY'],
+  :api_key      => ENV['stripe_api_key']
+}
+
