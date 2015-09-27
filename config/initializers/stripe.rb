2350fca90b5a0596eb3c2619e67ded00facb4f56
Konrad Kale <konrad.kale@icloud.com>
2015-09-27 03:56:38 +0000

    Before final

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,5 +1,2 @@
-Rails.configuration.stripe = {
-  :publishable_key => ENV['PUBLISHABLE_KEY'],
-  :api_key      => ENV['stripe_api_key']
-}
-
+Stripe.api_key = ENV["stripe_api_key"]
+STRIPE_PUBLIC_KEY = ENV["stripe_publishable_key"]
